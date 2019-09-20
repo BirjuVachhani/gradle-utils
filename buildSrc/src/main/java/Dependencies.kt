5592848f2d5a22e0b928f5f31e0aeada27aa584d
BirjuVachhani <brvachhani@gmail.com>
2019-09-20 20:00:55 +0530

    feat: add some basis structure information
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,28 @@
+/*
+ * Created by Birju Vachhani on 20 September 2019
+ * Copyright © 2019 Gradle Utils. All rights reserved.
+ */
+
+object Core {
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:1.3.50"
+}
+
+object AndroidX {
+    const val constraintLayout = ""
+    const val appCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val recyclerView = ""
+    const val viewPager = ""
+    const val viewPager2 = ""
+    const val fragmentKtx = ""
+    const val coreKtx = "androidx.core:core-ktx:1.1.0"
+}
+
+object Test {
+    const val junit4 = "junit:junit:4.12"
+}
+
+object AndroidTest {
+    const val runner = "androidx.test:runner:1.2.0"
+    const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+}
+ 
